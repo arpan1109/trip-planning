e711c0b180cf07a0d276261ff50c66b1a4eafa9e
Arpan <palarpan11092004@gmail.com>
2026-03-20 18:57:17 +0530

    Changing base url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
     // baseURL: 'http://localhost:5000/api', // Adjust to your server port
-    baseURL: 'https://trip-planning-3d722ynxp-arpan-s-projects-972e8302.vercel.app', // Adjust to your server port
+    baseURL: 'https://journeys-api.vercel.app/api', // Adjust to your server port
     withCredentials: true, // CRUCIAL: Sends the JWT cookie with every request
 });
 
